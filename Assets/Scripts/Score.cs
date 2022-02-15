928e97966bc4767f82857dea47a68b51c6ce4140
David Buckley <runalldayandnight@gmail.com>
2022-02-15 15:35:03 -0700

    Slower speed

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,8 +7,10 @@ public class Score : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Scene1")
+        // if it's the zeroth scene (buildIndex == 0)
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 0)
         {
+            // reset the score
             PlayerPrefs.SetInt("score", 0);
         }
         GetComponent<UnityEngine.UI.Text>().text = PlayerPrefs.GetInt("score").ToString();
